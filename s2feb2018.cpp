5beb3500264f381c8979aefacf2b17b479c847d6
Joelle Yang <94316082+d3ltaV@users.noreply.github.com>
2024-08-31 14:59:09 -0400

    Update s2feb2018.cpp

--- s2feb2018.cpp
+++ s2feb2018.cpp
@@ -1,37 +1,54 @@
-#include <cmath>
+#include <iostream>
 #include <vector>
-#include <fstream>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
-//Naive recursion first
+// Naive recursion; number of discarded boots
 int step(vector<int> tiles, vector<int> depth, vector<int> dist, int pointerTiles, int pointerBoots, int n, int b) {
     if (pointerTiles == n - 1) {
         return 0;
     } 
-    if (tiles.at(pointerTiles) >  )
-    for (int i = 0; i < n; i++) {
-        return step(tiles, depth, )
+    if (pointerBoots == b) {
+        return INT_MAX;
     }
-}   
+    int min_discarded = INT_MAX;
+    
+    // Option 1: Switch boots
+    if (tiles[pointerTiles] > depth[pointerBoots]) {
+        int temp = 1 + step(tiles, depth, dist, pointerTiles, pointerBoots + 1, n, b);
+        min_discarded = min(min_discarded, temp);
+    } else {
+        for (int i = 1; i <= dist[pointerBoots] && pointerTiles + i < n; i++) {
+            if (tiles[pointerTiles + i] <= depth[pointerBoots]) {
+                int temp2 = 1 + step(tiles, depth, dist, pointerTiles + i, pointerBoots + 1, n, b);
+                min_discarded = min(min_discarded, temp2);
+            }
+        }
+    }
+    
+    return min_discarded;
+}
+
 int main() {
-    ifstream fin("snowboots.in");
-    ofstream fout("snowboots.out");
     int n, b;
-    fin >> n >> b;
-    vector<int> tiles;
-    vector<int> depth;
-    vector<int> dist;
+    cin >> n >> b;
+
+    vector<int> tiles(n);
+    vector<int> depth(b);
+    vector<int> dist(b);
+
     for (int i = 0; i < n; i++) {
-        int a;
-        fin >> a;
-        tiles.push_back(a);
+        cin >> tiles[i];
     }
-    for (int i = 0; i < b;) {
-        int a, b;
-        fin >> a >> b;
-        depth.push_back(a);
-        dist.push_back(b);
+    for (int i = 0; i < b; i++) {
+        cin >> depth[i] >> dist[i];
     }
+
+    int minDiscardedBoots = step(tiles, depth, dist, 0, 0, n, b);
+    
+    cout << minDiscardedBoots << endl;
+
+    return 0;
 }
