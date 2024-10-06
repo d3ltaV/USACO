b1ebc787d703eec85149290011ce3ff6b94690aa
BuildTools <unconfigured@null.spigotmc.org>
2024-10-06 14:26:09 -0400

    add

--- Main.cpp
+++ Main.cpp
@@ -1,137 +1,86 @@
-
-
-#include <iostream>
+#include <vector>
+#include <unordered_map>
 #include <queue>
+#include <iostream>
+#include <set>
+#include <algorithm>
+
 using namespace std;
 
-const int MAXN = 100010;
-int N; // number of cows
-int buddy_of[MAXN]; // which cow each points to
-int value[MAXN]; // how many times each cow moos
-
-int done[MAXN]; // which ones are already removed
-int in_degree[MAXN]; // keep track of in-degree as we process nodes
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> buddy_of[i] >> value[i];
-        buddy_of[i]--;
-    }
-    for (int i = 0; i < N; i++) {
-        in_degree[buddy_of[i]]++;
-    }
-    queue<int> q;
-    for (int i = 0; i < N; i++) {
-        if (in_degree[i] == 0) {
-            q.push(i);
-        }
-    }
-    long long ans = 0;
-    while (!q.empty()) {
-        int x = q.front();
-        q.pop();
-        // mark x as done
-        done[x] = true;
-        // update ans
-        ans += value[x];
-        in_degree[buddy_of[x]]--;
-        if (in_degree[buddy_of[x]] == 0) {
-            q.push(buddy_of[x]);
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        if (!done[i]) { //if node has an an in degree > 0, it must be a part of a cycle. For every node that has in degree more than 1, one node must have in degree 0 (for every extra in degree). those nodes are removed!
-            // found a cycle
-            // loop over the cycle
-            int start = i, curr = start;
-            long long cycle_sum = 0;
-            int smallest_value = value[start];
-            do {
-                cycle_sum += value[curr];
-                smallest_value = min(smallest_value, value[curr]);
-                done[curr] = true;
-                curr = buddy_of[curr]; 
-            } while (curr != start);
+    int n;
+    cin >> n;
 
-            ans += cycle_sum;
-            ans -= smallest_value;
+    vector<int> pots(n, 0);
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        pots[i] = temp - 1;
+    }
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;  
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    //leaf nodes
+    vector<int> leaves;
+    for (int i = 1; i < n; i++) {  //no root
+        if (adj[i].size() == 1) {
+            leaves.push_back(i);
         }
     }
-    cout << ans << "\n";
+
+    unordered_map<int, int> parents;
+    vector<vector<int>> ancestors(n);
+    queue<int> q;
+    vector<bool> visited(n, false);
+    visited[0] = true;
+    parents[0] = -1;
+    q.push(0);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for (int child : adj[node]) {
+            if (!visited[child]) {
+                parents[child] = node;
+                visited[child] = true;
+                q.push(child);
+                ancestors[child] = ancestors[node];
+                ancestors[child].push_back(node);
+            }
+        }
+    }
+    int shortest=leaves.size();
+    pots.resize(shortest);
+    //for some reason this doens twork even though its the same greedy idea as the solution; the solution just simulates it.
+    // Calculate closest pot for each leaf
+    vector<int> closest(n, -1);
+    for (int i = 0; i < leaves.size(); i++) {
+        int leaf = leaves[i];
+        if (find(pots.begin(), pots.end(), leaf) != pots.end()) {
+            closest[leaf] = leaf;
+            pots.erase(find(pots.begin(), pots.end(), leaf));
+        } else {
+            for (int j = 0; j < ancestors[leaf].size(); j++) {
+                if (find(pots.begin(), pots.end(), ancestors[leaf][j]) != pots.end()) {
+                    closest[leaf] = ancestors[leaf][j];
+                    pots.erase(find(pots.begin(), pots.end(), ancestors[leaf][j]));
+                    break;
+                }
+            }
+        }
+    }
+    set<int> distinct;
+    for (int i = 0; i < closest.size(); i++) {
+        if (closest[i] != -1) {
+            distinct.insert(closest[i]);
+        }
+    }
+    int ans = distinct.size();
+    cout << ans;
 
     return 0;
 }
-
-// #include <cstdio>
-// #include <iostream>
-// #include <vector>
-// #include <cmath>
-// #include <climits>
-// #include <algorithm>
-// using namespace std;
-// // DSU for all connected components of graph (cycles); for each cycle, add the sum of edges - minimum edge;
-// //DSU only works fo undirected graphs; DFS for all cyclic components of graph.
-// //Mistake: didn't realize that in degrees could be zero; thought that it was all cycles. If in degree = 0, immediately add to result out degree value because it doesn't affect the cycles.
-
-//this dfs is too slow
-// vector<bool> visited;
-// pair<int, int> dfs(int node, vector<vector<pair<int, int>>> &adj, int &total, int &min, int parent) {
-//     visited[node] = true;
-//     for (pair<int, int> child : adj.at(node)) {
-//         if (!visited[child.first]) {
-//             total += child.second;
-//             if (child.second < min) {
-//                 min = child.second;
-//             }
-//             dfs(child.first, adj, total, min, parent);
-//         } else if (child.first == parent) {
-//             total += child.second;
-//             if (child.second < min) {
-//                 min = child.second;
-//             }
-//         }
-//     }
-//     return make_pair(total, min);
-// }
-// int main() {
-//     int n;
-//     int ans = 0;
-//     visited.resize(n, false);
-//     scanf("%d", &n);
-//     vector<vector<pair<int, int>>> adj(n, vector<pair<int, int>>());
-//     for (int i = 0; i < n; i++) {
-//         int a, b;
-//         scanf("%d %d", &a, &b);
-//         adj.at(i).push_back(make_pair(a - 1, b));
-//     }
-//     vector<int> inDegrees(n, 0);
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < adj.at(i).size(); j++) {
-//             inDegrees.at(adj.at(i).at(j).first) += 1;
-//         }
-//     }
-///     USE STACKS!!!
-//     for (int i = 0; i < n; i++) {
-//         if (inDegrees.at(i) == 0) {
-//             int a = adj.at(i).at(0).second;
-//             ans += a;
-//             visited[i] = true;
-//             inDegrees.at(adj.at(i).at(0).first) -= 1;
-//             if (inDegrees.at(adj.at(i).at(0).first) == 0) {
-//                 int b = adj.at(adj.at(i).at(0).first).at(0).f
-//             }
-//         }
-//     }
-//     for (int i = 0; i < n; i++) {
-//         if (!visited[i]) {
-//             int totalWeight = 0;
-//             int minEdge = INT_MAX;
-//             pair<int, int> result = dfs(i, adj, totalWeight, minEdge, i);
-//             // cout << result.first <<" " << result.second << " ";
-//             ans += (result.first - result.second);
-//         }
-//     }
-//     printf("%d", ans);
-// }
--- s2jan2024.cpp
+++ s2jan2024.cpp
@@ -1,28 +1,156 @@
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
 #include <queue>
 #include <iostream>
+#include <set>
+#include <algorithm>
+
 using namespace std;
+
 int main() {
     int n;
     cin >> n;
+
     vector<int> pots(n, 0);
     for (int i = 0; i < n; i++) {
-        cin >> pots[i];
+        int temp;
+        cin >> temp;
+        pots[i] = temp - 1;
     }
     vector<vector<int>> adj(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
-        adj[a].push_back(b - 1); //0-indexing
-        adj[b].push_back(a - 1);
+        a--; b--;  
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
+    //leaf nodes
     vector<int> leaves;
-    for (int i = 1; i < n; i++) { //exclude node 1
+    for (int i = 1; i < n; i++) {  //no root
         if (adj[i].size() == 1) {
             leaves.push_back(i);
         }
     }
-    unordered_map<int, int> ancestors;
+
+    unordered_map<int, int> parents;
+    vector<vector<int>> ancestors(n);
+    queue<int> q;
+    vector<bool> visited(n, false);
+    visited[0] = true;
+    parents[0] = -1;
+    q.push(0);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for (int child : adj[node]) {
+            if (!visited[child]) {
+                parents[child] = node;
+                visited[child] = true;
+                q.push(child);
+                ancestors[child] = ancestors[node];
+                ancestors[child].push_back(node);
+            }
+        }
+    }
+    int shortest=leaves.size();
+    pots.resize(shortest);
+    //for some reason this doens twork even though its the same greedy idea as the solution; the solution just simulates it.
+    // Calculate closest pot for each leaf
+    vector<int> closest(n, -1);
+    for (int i = 0; i < leaves.size(); i++) {
+        int leaf = leaves[i];
+        if (find(pots.begin(), pots.end(), leaf) != pots.end()) {
+            closest[leaf] = leaf;
+            pots.erase(find(pots.begin(), pots.end(), leaf));
+        } else {
+            for (int j = 0; j < ancestors[leaf].size(); j++) {
+                if (find(pots.begin(), pots.end(), ancestors[leaf][j]) != pots.end()) {
+                    closest[leaf] = ancestors[leaf][j];
+                    pots.erase(find(pots.begin(), pots.end(), ancestors[leaf][j]));
+                    break;
+                }
+            }
+        }
+    }
+    set<int> distinct;
+    for (int i = 0; i < closest.size(); i++) {
+        if (closest[i] != -1) {
+            distinct.insert(closest[i]);
+        }
+    }
+    int ans = distinct.size();
+    cout << ans;
+
+    return 0;
 }
+
+// #include <vector>
+// #include <unordered_map>
+// #include <unordered_set>
+// #include <queue>
+// #include <iostream>
+// #include <algorithm>
+// #include <set>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
+//     vector<int> pots(n, 0);
+//     for (int i = 0; i < n; i++) {
+//         cin >> pots[i];
+//     }
+//     vector<vector<int>> adj(n);
+//     for (int i = 0; i < n; i++) {
+//         int a, b;
+//         cin >> a >> b;
+//         adj[a].push_back(b - 1); //0-indexing
+//         adj[b].push_back(a - 1);
+//     }
+//     vector<int> leaves;
+//     for (int i = 1; i < n; i++) { //exclude node 1
+//         if (adj[i].size() == 1) {
+//             leaves.push_back(i);
+//         }
+//     }
+//     unordered_map<int, int> parents;
+//     vector<vector<int>> ancestors(n);
+//     queue<int> q;
+//     vector<bool> visited(n, false);
+//     int root = 0;
+//     visited[root] = true;
+//     parents[root] = -1;
+//     q.push(root);
+//     while (!q.empty()) {
+//         int node = q.front();
+//         q.pop();
+//         for (int child: adj[node]) {
+//             if (!visited[child]) {
+//                 parents[child] = node;
+//                 visited[child] = true;
+//                 q.push(child);
+//             }
+//             ancestors[child] = ancestors[node];
+//             ancestors[child].push_back(node);
+//         }
+//     }
+//     int shortest=leaves.size();
+//     pots.resize(shortest);
+//     vector<int> closest(n, -1);
+//     for (int i = 0; i < leaves.size(); i ++) {
+//         if (find(pots.begin(), pots.end(), leaves[i]) != pots.end()) {
+//             closest[i] = leaves[i];
+//         } else {
+//             for (int j = 0; i < ancestors[i].size(); j++) {
+//                 if (find(pots.begin(), pots.end(), ancestors[i][j]) != pots.end()) {
+//                     closest[i] = ancestors[i][j];
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     set<int> distinct(closest.begin(), closest.end());
+//     int ans = distinct.size();
+//     cout << ans;
+
+// }
